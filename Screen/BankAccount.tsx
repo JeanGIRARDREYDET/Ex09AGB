c2cba2d5094ecbcc2cfa55a8a33c5db1c53a08c8
JeanGIRARDREYDET <cogitafac@gmail.com>
2022-06-23 09:32:16 +0200

    tempo qui ne marche pas

--- Screen/BankAccount.tsx
+++ Screen/BankAccount.tsx
@@ -1,20 +1,21 @@
 import * as React from 'react'
-import { Text, View, StyleSheet ,FlatList} from 'react-native'
+import { Text, View, StyleSheet, FlatList,Pressable } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Navigation from '../Navigation/Navigation'
 import axios from 'axios'
-import AccountLigneJsonData from '..//data/E09_data.json'; // This import style requires "esModuleInterop", see "side notes"
+import AccountLigneJsonData from '../data/E09_data.json'; // This import style requires "esModuleInterop", see "side notes"
 
 import { ScrollView } from 'react-navigation'
 //console.log(AccountLigneJson);
 //const Tab = createBottomTabNavigator();
-import Realm from "realm";
+
+import { Realm, createRealmContext } from '@realm/react'
 
 const TaskSchema = {
   name: "Task",
   properties: {
-    _id: "int",
+    _id: "objectId",
     name: "string",
     status: "string?",
   },
@@ -23,34 +24,21 @@ const TaskSchema = {
 // open a local realm with the 'Cat' schema
 
 
-
-
-class BookSchema extends Realm.Object {}
-BookSchema.schema = {
-    name: 'Book',
-    properties: {
-        title: 'string',
-        pages:  'int',
-        edition: 'int?'
-    }
-};
-class AccountRow extends Realm.Object {}
+class AccountRow extends Realm.Object { }
 
 AccountRow.schema = {
   name: 'AccountRow',
   properties: {
-    id: 'objectId',
+    id: 'string',
     type: 'string',
     date: 'string',
     amount: 'string',
     category: 'string',
     comments: 'string',
-    userid :'string',
-  },
+    userid: 'string',
+  }
 };
 
-let realm  = new Realm({schema: [BookSchema], schemaVersion: 1});
-//let realmA = new Realm({schema: [BookSchema], schemaVersion: 1});
 
 
 
@@ -58,146 +46,130 @@ let realm  = new Realm({schema: [BookSchema], schemaVersion: 1});
 
 
 
- /*
+/*
 function ReadJson(AccountLigneJsonData: obj, sufix: string) {
-  
-  let accountRows: accountRow[] = [];
-  let curentAccountLine = new accountRow();
-  AccountLigneJsonData.forEach(obj => {
-    Object.entries(obj).forEach(([key, value]) => {
-      if ((typeof value) === "object") {
-        
-        const tmp = ReadJson(value, sufix);
-        console.log(key);
-        
-        console.log(tmp);
-        accountRows.push(curentAccountLine);
-      } else {
-        let svalue = "" + String(value);
-        if ((typeof value) === "string") {
-          if (key === '_id') {
-            curentAccountLine.userid;
-          } else if (key === 'user') {
-            curentAccountLine.user = svalue;
-          } else if (key === 'date') {
-            curentAccountLine.date = svalue;
-          } else if (key === 'amount') {
-            curentAccountLine.amount = svalue;
-          } else if (key === 'category') {
-            curentAccountLine.category = svalue;
-          } else if (key === 'comments') {
-            curentAccountLine.comments = svalue;
-          } else if (key === '_id_income') {
-            curentAccountLine.type = "income";
-            curentAccountLine.id = "i"+svalue;
-          } else if (key === '_id_expense') {
-            curentAccountLine.id = svalue;
-            curentAccountLine.type = "expense";
-          }
-        }
-      }
-    });
+ 
+ let accountRows: accountRow[] = [];
+ let curentAccountLine = new accountRow();
+ AccountLigneJsonData.forEach(obj => {
+   Object.entries(obj).forEach(([key, value]) => {
+     if ((typeof value) === "object") {
+       
+       const tmp = ReadJson(value, sufix);
+       console.log(key);
+       
+       console.log(tmp);
+       accountRows.push(curentAccountLine);
+     } else {
+       let svalue = "" + String(value);
+       if ((typeof value) === "string") {
+         if (key === '_id') {
+           curentAccountLine.userid;
+         } else if (key === 'user') {
+           curentAccountLine.user = svalue;
+         } else if (key === 'date') {
+           curentAccountLine.date = svalue;
+         } else if (key === 'amount') {
+           curentAccountLine.amount = svalue;
+         } else if (key === 'category') {
+           curentAccountLine.category = svalue;
+         } else if (key === 'comments') {
+           curentAccountLine.comments = svalue;
+         } else if (key === '_id_income') {
+           curentAccountLine.type = "income";
+           curentAccountLine.id = "i"+svalue;
+         } else if (key === '_id_expense') {
+           curentAccountLine.id = svalue;
+           curentAccountLine.type = "expense";
+         }
+       }
+     }
+   });
 
 
 
-  });
-  return accountRows;
+ });
+ return accountRows;
 }
 */
 
 
-realm.write(() => {
-  const book = realm.create('Book', {
-    title: 'Barry Butter' ,
-    pages:  400
-  });
-});
+
+// crée une ligne de compte
 
 
+/*
 
-let getAllBooks = () => {
-  return realm.objects('Book');
-}
+ */
 
 
-const Home = () => {
-  const array = [
-    {
+function Home() {
+  console.log(Realm.defaultPath)
+  let realm = new Realm({ schema: [AccountRow], schemaVersion: 5});
+
+  realm.write(() => {
+    const accountRow = realm.create('AccountRow', {
       id: '1',
-      user: 'example title 1',
-      amount: 'example subtitle 1',
-    },
-    {
-      id: '2',
-      user: 'example title 2',
-      amount: 'example subtitle 2',
-    },
-    {
-      id: '3',
-      user: 'example title 3',
-      amount: 'example subtitle 3',
-    },
-  ];
-  
- // const array = ReadJson(AccountLigneJsonData, "");
-
-
-
-
-
-// Add a couple of Tasks in a single, atomic transaction
-
-
-
-
-
-
-  const list = () => {
-    return array.map((element) => {
-      return (
-       <View>
-        <View key={element.id} style={{margin: 10}}>
-          <Text>{element.user}</Text>
-          <Text>{element.amount}</Text>
-        </View>
-        <FlatList
-    data={getAllBooks()}
-    keyExtractor={(item, index) => index.toString()}
-    renderItem={({item, index}) => {
-        return (
-            <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-                <Text>{item.title}</Text>
-                <Text>{item.pages}</Text>
-                <Text>{item.edition === null ? 'null' : item.edition}</Text>
-            </View>
-        )
-    }} />
-
-        </View>
-      );
+      type: 'In',
+      date: '12/12/2022',
+      amount: '15,33',
+      category: 'Pension Amimentaire',
+      comments: 'string',
+      userid :'Jean',
+    
     });
-  };
-
-  return <View>{list()}</View>;
-};
+  });
 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-  },
-  error: {
-    color: "red",
-    marginBottom: 10,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#4e4e4e',
-    padding: 12,
-    margin: 5,
+  let getAllAccountRows = () => {
+    return realm.objects('AccountRow');
   }
 
-})
-export default Home;
+    return <View>
+
+    <FlatList
+      data={getAllAccountRows()}
+      keyExtractor={(item, index) => index.toString()}
+      renderItem={({ item, index }) => {
+        return (
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+            
+            <Pressable
+              onPress={() => {
+                realm.write(() => {
+                  realm.delete(item)
+                })
+              }} ><Text>{"🗑️"}</Text></Pressable>
+            <Text>-{item.amount === null ? 'null' : item.edition}-</Text>
+            <Text>+{item.type}</Text>
+            <Text>{item.category}</Text>
+            <Text>{item.amount === null ? 'null' : item.edition}</Text>
+          </View>
+        )
+      }} />
+
+  </View>;
+  };
+
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 20,
+    },
+    error: {
+      color: "red",
+      marginBottom: 10,
+    },
+    input: {
+      borderWidth: 1,
+      borderColor: '#4e4e4e',
+      padding: 12,
+      margin: 5,
+    }
+
+  }
+  )
+
+  export default Home;
 
